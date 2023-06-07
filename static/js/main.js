14c3778aca7dd779e8c2aa49a2d845ca4ae334f9
Saylee Jagtap <sjagtap@pdx.edu>
2023-06-07 14:51:45 -0700

    Did few changes according to Wave

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
 
+function removeEmptyLink() {
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        if (link.getAttribute('href') === '') {
+            link.parentNode.removeChild(link);
+        }
+    }
+}
