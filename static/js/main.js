fc51dff2ae6f93b49c7f6abb1c7dfa774f5894c2
Saylee Jagtap <sjagtap@pdx.edu>
2023-06-07 17:00:31 -0700

    Added code for Project and Contact Me

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,38 @@
+// Navigation effect on scroll
 
-function removeEmptyLink() {
-    var links = document.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        if (link.getAttribute('href') === '') {
-            link.parentNode.removeChild(link);
-        }
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    let sectionHeigth = current.offsetHeight;
+    let sectionTop = current.offsetTop - 40;
+    let id = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeigth) {
+      document
+        .querySelector(".navigation-items a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".navigation-items a[href*=" + id + "]")
+        .classList.remove("active");
     }
-}
+  });
+});
+
+const menuButton = document.querySelector("navigation-menu-button");
+const closeButton = document.querySelector("navigation-close-button");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+  navigation.classList.add("active");
+});
+closeButton.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
